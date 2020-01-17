392081f1ce691a24bf802a176ca2b1eb84ecff0c
Mariia Glazova <59918542+MarriaGlazova@users.noreply.github.com>
2020-01-17 17:25:35 +0300

    add conflict

--- src/main/java/com/example/demo/SimpleServiceImpl.java
+++ src/main/java/com/example/demo/SimpleServiceImpl.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
 public class SimpleServiceImpl implements SimpleService {
+
+    @Value("${spring.application.name}")
+    String name;
+
     @Override
     public void doSomething() {
-        
+        Logger logger = LoggerFactory.getLogger(SimpleService.class);
+        logger.info("User Id = {}.", name);
     }
 }
