d81228b3ff6aa11fe3cfe7c33ba6d32493d9ed66
Mariia Glazova <think.about.sleeping@gmail.com>
2020-01-17 21:13:50 +0300

    add setters and getters

--- src/main/java/com/example/demo/ConfigProperties.java
+++ src/main/java/com/example/demo/ConfigProperties.java
@@ -6,6 +6,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "default")
 public class ConfigProperties {
+    String name;
+    String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
 
 }
