ac390a34b30fbd5720207ecca3fe72335922545b
Mariia Glazova <59918542+MarriaGlazova@users.noreply.github.com>
2020-01-17 15:58:23 +0300

    add logging(SimpleController.java)

--- src/main/java/com/example/demo/SimpleController.java
+++ src/main/java/com/example/demo/SimpleController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,6 +26,11 @@ public class SimpleController {
         String SOME_KEY = "KEY";
 
         if (SOME_KEY.equalsIgnoreCase(key)) {
+            Logger logger = LoggerFactory.getLogger(SimpleRequest.class);
+            logger.info("UserId = {}.", request.getUserId());
+            logger.info("Name = {}.", request.getName());
+            logger.info("SomeData = {}.", request.getSomeData());
+
             response = new BaseResponse(SUCCESS_STATUS, SUCCESS_STATUS_CODE);
         } else {
             response = new BaseResponse(ERROR_STATUS, ERROR_STATUS_CODE);
