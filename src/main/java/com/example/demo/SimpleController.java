4a981b5986727b952a82fa8725deb70b99ba9686
Mariia Glazova <59918542+MarriaGlazova@users.noreply.github.com>
2020-01-17 18:45:45 +0300

    add @Autowired Service

--- src/main/java/com/example/demo/SimpleController.java
+++ src/main/java/com/example/demo/SimpleController.java
@@ -2,17 +2,21 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/simple")
 
 public class SimpleController {
+
     private static final String SOME_KEY = "KEY";
     private static final String SUCCESS_STATUS = "success";
     private static final String ERROR_STATUS = "error";
     private static final int SUCCESS_STATUS_CODE = 1;
     private static final int ERROR_STATUS_CODE = -1;
+    @Autowired
+    SimpleServiceImpl simpleService;
 
     @GetMapping
     public BaseResponse showStatus() {
@@ -23,7 +27,7 @@ public class SimpleController {
     public BaseResponse pay(@RequestParam(value = "key") String key, @RequestBody SimpleRequest request) {
 
         final BaseResponse response;
-        
+
         if (SOME_KEY.equalsIgnoreCase(key)) {
             Logger logger = LoggerFactory.getLogger(SimpleRequest.class);
             logger.info("UserId = {}.", request.getUserId());
--- src/main/java/com/example/demo/SimpleServiceImpl.java
+++ src/main/java/com/example/demo/SimpleServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleServiceImpl implements SimpleService {
+    
     @Override
     public void doSomething() {
-        
+
     }
 }
